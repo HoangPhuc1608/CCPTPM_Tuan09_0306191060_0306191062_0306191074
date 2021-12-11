c37959286424716612065529b50744809248416e
TripleT511 <0306191074@caothang.edu.vn>
2021-12-11 20:05:40 +0700

    Create clsPost and clsTopic

---
+++ BT_Tuan09/Models/Post.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BT_Tuan09.Models
+{
+    public class Post
+    {
+        private static int postId;
+        private static string title;
+        private static string image;
+        private static string content;
+        private static string typeTopic;
+        private static int status;
+
+        public static int PostId { get => postId; set => postId = value; }
+        public static string Title { get => title; set => title = value; }
+        public static string Image { get => image; set => image = value; }
+        public static string Content { get => content; set => content = value; }
+        public static string TypeTopic { get => typeTopic; set => typeTopic = value; }
+        public static int Status { get => status; set => status = value; }
+
+        public Post(int _id, string _title, string _image, string _content, string _typeTopic, int _status)
+        {
+            postId = _id;
+            title = _title;
+            image = _image;
+            content = _content;
+            typeTopic = _typeTopic;
+            status = _status;
+        }
+
+        public static List<Post> searchPost(string title)
+        {
+        }
+
+        public static void createPost()
+        {
+
+        }
+
+        public static bool updatePost(Post post)
+        {
+        }
+
+        public static bool deletePost(int postId)
+        {
+        }
+    }
+}
---
+++ BT_Tuan09/Models/Topic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BT_Tuan09.Models
+{
+    public class Topic
+    {
+        private int idTopic;
+        private string nameTopic;
+        private string typeTopic;
+
+        public int IdTopic { get => idTopic; set => idTopic = value; }
+        public string NameTopic { get => nameTopic; set => nameTopic = value; }
+        public string TypeTopic { get => typeTopic; set => typeTopic = value; }
+
+        public Topic(int idTopic, string nameTopic, string typeTopic)
+        {
+            this.IdTopic = idTopic;
+            this.NameTopic = nameTopic;
+            this.TypeTopic = typeTopic;
+        }
+
+        public static List<Topic> searchTopic(string nameTopic)
+        {
+        }
+
+        public static void createTopic()
+        {
+
+        }
+
+        public static bool updateTopic(Topic topic)
+        {
+        }
+
+        public static bool deleteTopic(int topicId)
+        {
+        }
+    }
+}
