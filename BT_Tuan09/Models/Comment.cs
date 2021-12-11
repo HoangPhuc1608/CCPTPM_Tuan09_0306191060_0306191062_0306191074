82bcd31fb00431a10216b8b717d79d399122e0ca
PhucSmRePo <0306191060@caothang.edu.vn>
2021-12-11 20:18:59 +0700

    Update Mem Comment 3

--- BT_Tuan09/Models/Comment.cs
+++ BT_Tuan09/Models/Comment.cs
@@ -14,14 +14,21 @@ namespace BT_Tuan09.Models
 		private int postID;
 		private int status;
 
-		public Comment(int _comentID, int _memberID, string _content, DateTime _commentDate, int _postID, int _status)
+        public int CommentID { get => commentID; set => commentID = value; }
+        public int MemberID { get => memberID; set => memberID = value; }
+        public string Content { get => content; set => content = value; }
+        public DateTime CommentDate { get => commentDate; set => commentDate = value; }
+        public int PostID { get => postID; set => postID = value; }
+        public int Status { get => status; set => status = value; }
+
+        public Comment(int _comentID, int _memberID, string _content, DateTime _commentDate, int _postID, int _status)
 		{
-			_comentID = commentID;
-			_memberID = memberID;
-			_content = content;
-			_commentDate = commentDate;
-			_postID = postID;
-			_status = status;
+			_comentID = CommentID;
+			_memberID = MemberID;
+			_content = Content;
+			_commentDate = CommentDate;
+			_postID = PostID;
+			_status = Status;
 
 		}
 
--- BT_Tuan09/Models/Member.cs
+++ BT_Tuan09/Models/Member.cs
@@ -15,14 +15,22 @@ namespace BT_Tuan09.Models
 		private string gender;
 		private int typeAccount;
 
-		public Member(int _id, string _username, string _pass, DateTime _dateofbirth, string _email, string _gender)
+        public int Id { get => id; set => id = value; }
+        public string UserName { get => userName; set => userName = value; }
+        public string PassWord { get => passWord; set => passWord = value; }
+        public DateTime DateOfBirth { get => dateOfBirth; set => dateOfBirth = value; }
+        public string Email { get => email; set => email = value; }
+        public string Gender { get => gender; set => gender = value; }
+        public int TypeAccount { get => typeAccount; set => typeAccount = value; }
+
+        public Member(int _id, string _username, string _pass, DateTime _dateofbirth, string _email, string _gender)
 		{
-			_id = id;
-			_username = userName;
-			_pass = passWord;
-			_dateofbirth = dateOfBirth;
-			_email = email;
-			_gender = gender;
+			_id = Id;
+			_username = UserName;
+			_pass = PassWord;
+			_dateofbirth = DateOfBirth;
+			_email = Email;
+			_gender = Gender;
 		}
 
 		public bool login(string username, string pass)
