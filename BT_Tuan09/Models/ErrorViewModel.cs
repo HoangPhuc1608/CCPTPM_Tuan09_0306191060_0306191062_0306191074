5fa5a2a94a22f049ad192a00fff29a414e5b353d
HoangPhuc1608 <phucvh2001@gmail.com>
2021-12-11 19:21:01 +0700

    source code

---
+++ BT_Tuan09/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BT_Tuan09.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
